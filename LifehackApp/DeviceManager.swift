f17bc6b470cfc0cd194b4306bfb07e3ef01ac33a
Aleksander blindheim <Aleksander42195@gmail.com>
2025-09-24 04:36:02 +0200

    fix: remove invalid force unwrap in getDeviceIdentifier() to satisfy Swift 5 compiler

--- LifehackApp/DeviceManager.swift
+++ LifehackApp/DeviceManager.swift
@@ -170,7 +170,7 @@ class DeviceManager {
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
             guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value))!)
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
         return identifier
     }
